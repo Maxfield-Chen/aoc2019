64bb95cc4261b60f783e7c8ffcbe8bf562695b7e
maxfield-chen <maxfieldchen@gmail.com>
2019-12-01 21:32:05 -0800

    Add data directory to wake file. Implement D1P1.

--- Day1/src/Main.hs
+++ Day1/src/Main.hs
@@ -1,5 +1,34 @@
 module Main where
 
+import           Text.ParserCombinators.Parsec
+import           Data.Maybe                    as Maybe
+import qualified Data.Map                      as M
+
+type Module = Int
+type Fuel = Int
+
+pModules :: Parser [Module]
+pModules = endBy pModule eol
+
+pModule :: Parser Module
+pModule = do
+  digits <- many1 digit
+  return (read digits)
+
+eol :: Parser Char
+eol = char '\n'
+
+parseModules :: String -> Either ParseError [Module]
+parseModules = parse pModules "(unknown)"
+
+calcFuel :: Module -> Fuel
+calcFuel moduleMass = floor (dModuleMass / 3) - 2
+  where dModuleMass = fromIntegral moduleMass :: Double
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  part1 <- readFile "/home/nihliphobe/projects/haskell/aoc2019/Day1/data/part1.txt"
+  case parseModules part1 of
+    Left err -> fail (show err)
+    Right modules -> print sumFuel
+      where sumFuel = sum (map calcFuel modules)
--- wake.sh
+++ wake.sh
@@ -3,6 +3,9 @@
 stack new $1 simple
 cp stack.yaml $1/stack.yaml
 cp template.cabal $1/$1.cabal
+mkdir $1/data
+touch $1/data/part1.txt
+touch $1/data/part2.txt
 
 # Fix incorrect executable name in template.cabal
 sed -i "s/aoc2018/$1/g" $1/$1.cabal
