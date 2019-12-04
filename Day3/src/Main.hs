58e6a5b2527a063265eb9a3a0234f44d2ceab170
maxfield-chen <maxfieldchen@gmail.com>
2019-12-04 11:15:50 -0800

    Working Day2 p2, needs optimization.

--- Day3/src/Main.hs
+++ Day3/src/Main.hs
@@ -8,6 +8,7 @@ import qualified Data.Function as F
 data Direction = U | R | D | L deriving (Enum,Show)
 type Distance = Int
 type Vector = (Direction, Distance)
+type StepCoord = (Int, Int, Int)
 type Coord = (Int, Int)
 
 pWires :: Parser [[Vector]]
@@ -43,32 +44,60 @@ directionToCoord direction = case direction of
                                D -> (0,-1)
                                L -> (-1,0)
 
-applyVector :: (Coord, S.Set Coord) -> Vector -> (Coord, S.Set Coord)
+applyVector :: (StepCoord, S.Set StepCoord) -> Vector -> (StepCoord, S.Set StepCoord)
 applyVector start (direction, distance) = (end, wire `S.union` S.fromList paths)
   where 
-        ((xs,ys),wire) = start
-        end = (xs + distance * xd, ys + distance*yd)
-        paths = map (\(x,y) -> (xs + x*xd, ys + y*yd)) deltas
-        deltas = zip [1..distance] [1..distance]
+        ((ss,xs,ys),wire) = start
+        end = (ss+distance, xs + distance * xd, ys + distance*yd)
+        paths = map (\d -> (ss + d, xs + d*xd, ys + d*yd)) deltas
+        deltas = [1..distance]
         (xd, yd) = directionToCoord direction
 
-vecToCoords :: [Vector] -> S.Set Coord
-vecToCoords vs =   ret `S.difference` S.singleton (0,0)
-  where (_,ret) = foldl applyVector ((0,0), S.singleton (0,0)) vs 
+scToC :: S.Set StepCoord -> S.Set Coord
+scToC = S.map (\(_,x,y) -> (x,y))
+
+vecToSC :: [Vector] -> S.Set StepCoord
+vecToSC vs =   ret `S.difference` S.singleton (0,0,0)
+  where (_,ret) = foldl applyVector ((0,0,0), S.singleton (0,0,0)) vs 
 
 cabDistance :: Coord -> Coord -> Distance
 cabDistance (p1, p2) (q1,q2) = abs (p1-q1) + abs (p2-q2)
+
+combineSteps :: S.Set StepCoord -> S.Set StepCoord -> (S.Set StepCoord,S.Set StepCoord)
+combineSteps w w' = (r,r')
+  where 
+    r = S.map (\sc@(s,x,y) -> (s + findMinMatch sc w',x,y)) w
+    r'= S.map (\sc@(s,x,y) -> (s + findMinMatch sc w,x,y)) w'
+
+findMinMatch :: StepCoord -> S.Set StepCoord -> Int
+findMinMatch (_,x,y) w = sel1 (getMin matches)
+  where 
+    matches = S.filter (\(_,x',y') -> x == x' && y == y') w  
+    getMin sc 
+      | sc == S.empty = (0,0,0)
+      | otherwise = S.findMin sc
+
+sel1 :: (a, b, c) -> a
+sel1 (r,_,_) = r
+sel2 :: (a, b, c) -> b
+sel2 (_,r,_) = r
+sel3 :: (a, b, c) -> c
+sel3 (_,_,r) = r
 
 main :: IO ()
 main = do
   input <- readFile "/home/nihliphobe/projects/haskell/aoc2019/Day3/data/part1.txt"
   case parseWires input of
     Left err -> fail (show err)
-    Right wires -> 
+    Right wires -> do
       print p1
+      print p2
       where 
             p1 = L.minimumBy (compare `F.on` fst) cabs
+            p2 = L.minimumBy (compare `F.on` sel1) stepCrosses
             cabs = map (\c -> (cabDistance (0,0) c, c)) (S.toList crosses)
-            crosses = w1 `S.intersection` w2
-            w1 = vecToCoords (head wires)
-            w2 = vecToCoords (last wires)
+            crosses =scToC w1 `S.intersection` scToC w2
+            stepCrosses = sw `S.intersection` sw'
+            w1 = vecToSC (head wires)
+            w2 = vecToSC (last wires)
+            (sw, sw') = combineSteps w1 w2
