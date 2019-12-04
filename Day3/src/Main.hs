b339b9301c8300347b8d7ef0048ffb9e98ecf22f
maxfield-chen <maxfieldchen@gmail.com>
2019-12-04 13:24:27 -0800

    Refactor using Map instead of sets. Much much more efficient.

--- Day3/src/Main.hs
+++ Day3/src/Main.hs
@@ -1,15 +1,17 @@
 module Main where
 
 import           Text.ParserCombinators.Parsec
-import qualified Data.Set as S
+import qualified Data.Map as M
 import qualified Data.List as L
 import qualified Data.Function as F
+import Data.Maybe (Maybe, fromMaybe)
 
 data Direction = U | R | D | L deriving (Enum,Show)
 type Distance = Int
 type Vector = (Direction, Distance)
-type StepCoord = (Int, Int, Int)
 type Coord = (Int, Int)
+type Step = Int
+type Wire = M.Map Coord Step
 
 pWires :: Parser [[Vector]]
 pWires = endBy pVectors eol
@@ -44,45 +46,22 @@ directionToCoord direction = case direction of
                                D -> (0,-1)
                                L -> (-1,0)
 
-applyVector :: (StepCoord, S.Set StepCoord) -> Vector -> (StepCoord, S.Set StepCoord)
-applyVector start (direction, distance) = (end, wire `S.union` S.fromList paths)
+applyVector :: (Coord, Step, Wire) -> Vector -> (Coord, Step, Wire)
+applyVector start (direction, distance) = (end,ss+distance, newWire)
   where 
-        ((ss,xs,ys),wire) = start
-        end = (ss+distance, xs + distance * xd, ys + distance*yd)
-        paths = map (\d -> (ss + d, xs + d*xd, ys + d*yd)) deltas
+        newWire = M.unionWith min wire (M.fromList paths)
+        ((xs,ys),ss,wire) = start
+        end = (xs + distance * xd, ys + distance*yd)
+        paths = map (\d -> ((xs + d*xd, ys + d*yd), ss + d)) deltas
         deltas = [1..distance]
         (xd, yd) = directionToCoord direction
 
-scToC :: S.Set StepCoord -> S.Set Coord
-scToC = S.map (\(_,x,y) -> (x,y))
-
-vecToSC :: [Vector] -> S.Set StepCoord
-vecToSC vs =   ret `S.difference` S.singleton (0,0,0)
-  where (_,ret) = foldl applyVector ((0,0,0), S.singleton (0,0,0)) vs 
+traceWire :: [Vector] -> Wire
+traceWire vs = M.delete (0,0) wire 
+  where (_,_,wire) = foldl applyVector ((0,0), 0, M.empty ) vs 
 
 cabDistance :: Coord -> Coord -> Distance
 cabDistance (p1, p2) (q1,q2) = abs (p1-q1) + abs (p2-q2)
-
-combineSteps :: S.Set StepCoord -> S.Set StepCoord -> (S.Set StepCoord,S.Set StepCoord)
-combineSteps w w' = (r,r')
-  where 
-    r = S.map (\sc@(s,x,y) -> (s + findMinMatch sc w',x,y)) w
-    r'= S.map (\sc@(s,x,y) -> (s + findMinMatch sc w,x,y)) w'
-
-findMinMatch :: StepCoord -> S.Set StepCoord -> Int
-findMinMatch (_,x,y) w = sel1 (getMin matches)
-  where 
-    matches = S.filter (\(_,x',y') -> x == x' && y == y') w  
-    getMin sc 
-      | sc == S.empty = (0,0,0)
-      | otherwise = S.findMin sc
-
-sel1 :: (a, b, c) -> a
-sel1 (r,_,_) = r
-sel2 :: (a, b, c) -> b
-sel2 (_,r,_) = r
-sel3 :: (a, b, c) -> c
-sel3 (_,_,r) = r
 
 main :: IO ()
 main = do
@@ -94,10 +73,10 @@ main = do
       print p2
       where 
             p1 = L.minimumBy (compare `F.on` fst) cabs
-            p2 = L.minimumBy (compare `F.on` sel1) stepCrosses
-            cabs = map (\c -> (cabDistance (0,0) c, c)) (S.toList crosses)
-            crosses =scToC w1 `S.intersection` scToC w2
-            stepCrosses = sw `S.intersection` sw'
-            w1 = vecToSC (head wires)
-            w2 = vecToSC (last wires)
-            (sw, sw') = combineSteps w1 w2
+            p2 = L.minimumBy (compare `F.on` snd) (M.toList jointSteps)
+            cabs = map (\c -> (cabDistance (0,0) c, c)) (M.keys i)
+            i =w `M.intersection` w'
+            i' =w' `M.intersection` w
+            jointSteps = M.mapWithKey (\k s -> s + fromMaybe 0 (M.lookup k i)) i'
+            w = traceWire (head wires)
+            w' = traceWire (last wires)
