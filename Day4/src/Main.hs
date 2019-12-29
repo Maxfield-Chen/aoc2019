d44bc2210c342687e88fb2a626e61447cf732b14
maxfield-chen <maxfieldchen@gmail.com>
2019-12-29 12:42:15 -0800

    Refactor code to use a single IntState record. Dramatically simplifies the type signatures and implementation.

--- Day4/src/Main.hs
+++ Day4/src/Main.hs
@@ -5,6 +5,11 @@ import           Data.List
 import           Data.Char                      ( digitToInt )
 import           Debug.Trace
 
+data IntState = IntState { pc :: PC
+                         , code :: [Op]
+                         , input :: [Op]
+                         , output :: [Op]}
+
 type Op = Int
 type PC = Int
 type Noun = Int
@@ -70,77 +75,69 @@ evalMode Position  n code = if n < length code
     ++ show code
     )
 
-eval4OpFunc :: (Op -> Op -> Op) -> [Op] -> [Op] -> [Mode] -> ([Op], PC)
-eval4OpFunc f code position@(_ : p1 : p2 : dest : _) (m1 : m2 : m3 : _) =
-  (replaceOp dest (f r1 r2) code, 4)
+eval4OpFunc :: (Op -> Op -> Op) -> IntState -> IntState
+eval4OpFunc f s = s { code = nextCode, pc = pc s + 4 }
  where
-  r1 = evalMode m2 p2 code
-  r2 = evalMode m1 p1 code
+  r2                       = evalMode m2 p2 (code s)
+  r1                       = evalMode m1 p1 (code s)
+  (i : p1 : p2 : dest : _) = drop (pc s) (code s)
+  (m1          : m2   : _) = modes
+  (nextOp, modes)          = opToInstruction i
+  nextCode                 = replaceOp dest (f r1 r2) (code s)
 
-evalOp1 :: [Op] -> [Op] -> [Mode] -> ([Op], PC)
+evalOp1 :: IntState -> IntState
 evalOp1 = eval4OpFunc (+)
 
-evalOp2 :: [Op] -> [Op] -> [Mode] -> ([Op], PC)
+evalOp2 :: IntState -> IntState
 evalOp2 = eval4OpFunc (*)
 
-evalOp3 :: [Op] -> [Op] -> [Op] -> [Mode] -> ([Op], PC)
-evalOp3 code position@(_ : p : _) input (m : _) =
-  (replaceOp p (head input) code, 2)
+evalOp3 :: IntState -> IntState
+evalOp3 s = s { code = nextCode, pc = pc s + 2, input = tail (input s) }
+ where
+  nextCode     = replaceOp p1 (head (input s)) (code s)
+  (_ : p1 : _) = drop (pc s) (code s)
 
-evalOp4 :: [Op] -> [Op] -> [Op] -> [Mode] -> (Op, PC)
-evalOp4 code position@(_ : p : _) input (m : _) = (evalMode m p code, 2)
+evalOp4 :: IntState -> IntState
+evalOp4 s = s { output = save : output s, pc = pc s + 2 }
+ where
+  (_, modes)   = opToInstruction i
+  (i : p1 : _) = drop (pc s) (code s)
+  save         = evalMode (head modes) p1 (code s)
 
 -- Note this function assumes opcode 99 is handled externally
-doOp :: PC -> [Op] -> [Op] -> ([Op], [Op]) -> (([Op], [Op]), ([Op], PC))
-doOp pc code position (input, output)
-  | op == 1
-  = ((input, output), evalOp1 code position modes)
-  | op == 2
-  = ((input, output), evalOp2 code position modes)
-  | op == 3
-  = let newInput = case input of
-          (_ : xs) -> xs
-          _        -> input
-    in  ((newInput, output), evalOp3 code position input modes)
-  | op == 4
-  = let (ret, pcInc) = evalOp4 code position input modes
-    in  ((input, ret : output), (code, pcInc))
-  | otherwise
-  = error ("Unknown Opcode detected: " ++ show op)
+runIntCode :: IntState -> IntState
+runIntCode s | op == 1   = runIntCode $ evalOp1 s
+             | op == 2   = runIntCode $ evalOp2 s
+             | op == 3   = runIntCode $ evalOp3 s
+             | op == 4   = runIntCode $ evalOp4 s
+             | op == 99  = s
+             | otherwise = error ("Unknown Opcode detected: " ++ show op)
  where
-  i@(op, modes) = opToInstruction nextOp
-  nextOp        = if pc < length code
-    then code !! max 0 pc
-    else error ("Cannot Read next op, PC:" ++ show pc ++ " | " ++ show code)
-
-step :: PC -> [Op] -> ([Op], [Op]) -> ([Op], [Op])
-step pc code (input, output)
-  | op == 99  = (output, code)
-  | otherwise = step (pc + pcInc) nextStep (nextInput, nextOutput)
- where
-  op = if pc < length code
-    then code !! max 0 pc
-    else error ("Cannot Read next op, PC:" ++ show pc ++ " | " ++ show code)
-  position = drop pc code
-  ((nextInput, nextOutput), (nextStep, pcInc)) =
-    doOp pc code position (input, output)
+  (op, _) = if pc s < length (code s)
+    then opToInstruction (code s !! max 0 (pc s))
+    else error
+      ("Cannot Read next op, PC:" ++ show (pc s) ++ " | " ++ show (code s))
 
 evaluateCode :: [Op] -> [Op] -> ([Op], [Op])
-evaluateCode code userInput = step 0 code (userInput, [])
+evaluateCode intCode userInput =
+  let endState = runIntCode (IntState 0 intCode userInput [])
+  in  (output endState, code endState)
 
 main :: IO ()
 main = do
   input1 <- readFile
     "/home/nihliphobe/projects/haskell/aoc2019/Day4/data/part1.txt"
-  --print (evaluateCode [04, 2, 99] [1])
-  --print (evaluateCode [3, 0, 3, 1, 99] [1, 2, 3])
-  --print (evaluateCode [4, 0, 4, 4, 99] [1, 2, 3])
+  print (evaluateCode [04, 2, 99] [1])
+  print (evaluateCode [3, 0, 3, 1, 99] [100, 101, 102])
+  print (evaluateCode [4, 0, 4, 4, 99] [1, 2, 3])
+  print (evaluateCode [104, 0, 4, 4, 99] [1, 2, 3])
   print (evaluateCode [1002, 0, 2, 3, 99] [1, 2, 3])
-  --print (evaluateCode [3, 0, 2, 0, 2, 3, 99] [1, 2, 3, 4])
-  --print (evaluateCode [3, 0, 2, 0, 2, 3, 4, 0, 99] [1, 2, 3, 4])
-  --print (evaluateCode [101, 5, 2, 0, 3, 1, 4, 0, 99] [1, 2, 3, 4])
+  print (evaluateCode [3, 0, 2, 0, 2, 3, 99] [1, 2, 3, 4])
+  print (evaluateCode [3, 0, 2, 0, 2, 3, 4, 0, 99] [1, 2, 3, 4])
+  print (evaluateCode [101, 5, 2, 0, 3, 1, 4, 0, 99] [99, 2, 3, 4])
   case parseOps input1 of
     Left  err     -> fail (show err)
     Right program -> do
       print part1
+      print "Done"
       where part1 = evaluateCode program [1]
