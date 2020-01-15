e5df1ae5a33e88ae3faa2e9e6f3470f751a28e0a
maxfield-chen <maxfieldchen@gmail.com>
2020-01-15 08:34:07 -0800

    attempt to revise code for efficiency, still getting the wrong answer.

--- Day11/src/Main.hs
+++ Day11/src/Main.hs
@@ -3,9 +3,10 @@ module Main where
 import qualified IntCode                       as I
 import           Data.Ord
 import           Data.List
-import           Control.Lens
+import qualified Data.Map.Strict               as M
 
 data Direction = North | East | South | West deriving (Show, Eq, Enum, Bounded)
+data Color = White | Black deriving (Show, Eq)
 
 
 right :: Direction -> Direction
@@ -23,51 +24,56 @@ doTurn end loop f d | d == end  = loop
                     | otherwise = f d
 
 dirToIncrement :: Direction -> (Int, Int)
-dirToIncrement d | d == North = (0, -1)
+dirToIncrement d | d == North = (0, 1)
                  | d == East  = (1, 0)
-                 | d == South = (0, 1)
+                 | d == South = (0, -1)
                  | d == West  = (-1, 0)
-
-colorAtPosition :: PainterBot -> Int
-colorAtPosition p = let (x, y) = position p in (ship p !! y) !! x
 
 fmap' :: (Int -> Int -> Int) -> (Int, Int) -> (Int, Int) -> (Int, Int)
 fmap' f (x, y) (x', y') = (f x x', f y y')
 
-maximumSideLength = 5000
-
-data PainterBot = PainterBot { ship :: [[Int]]
-                             , direction :: Direction
+data PainterBot = PainterBot { direction :: Direction
                              , position :: (Int, Int)
-                             , program :: I.IntState} deriving Show
+                             , program :: I.IntState
+                             , pathLog :: M.Map (Int, Int) Color} deriving Show
+
+colorAtPosition :: (Int, Int) -> M.Map (Int, Int) Color -> Color
+colorAtPosition = M.findWithDefault Black
+
+colorToInt :: Color -> Int
+colorToInt c | c == Black = 0
+             | c == White = 1
+
+intToColor :: Int -> Color
+intToColor i | i == 0    = Black
+             | i == 1    = White
+             | otherwise = error ("Unknown Color: " ++ show i)
+
 
 stepPainterBot :: PainterBot -> PainterBot
 stepPainterBot bot | I.status (program bot) == I.Halt = bot
                    | otherwise                        = stepPainterBot nextBot
  where
-  shipColor           = colorAtPosition bot
-  toRun               = (program bot) { I.input = [shipColor] }
+  shipColor           = colorAtPosition (position bot) (pathLog bot)
+  toRun               = (program bot) { I.input = [colorToInt shipColor] }
   nextProg            = I.runIntCode toRun
   paintColor : lr : _ = I.output nextProg
   (x, y)              = position bot
-  nextShip            = ship bot & element y . element x .~ paintColor
+  nextLog = M.insert (position bot) (intToColor paintColor) (pathLog bot)
   nextPos = fmap' (+) (position bot) (dirToIncrement (direction bot))
   nextDir | lr == 0   = left (direction bot)
           | lr == 1   = right (direction bot)
           | otherwise = error "Bot attempted to turn in an invalid direction."
-  nextBot = PainterBot nextShip nextDir nextPos nextProg
+  nextBot = PainterBot nextDir nextPos nextProg nextLog
 
 main :: IO ()
 main = do
   code <- readFile I.fileName
   case I.parseOps code of
     Left  err     -> fail (show err)
-    Right program -> print squaresPainted
+    Right program -> do
+      print $ pathLog spentBot
+      print $ length (pathLog spentBot)
      where
-      squaresPainted =
-        foldl (\a xs -> a + length xs) 0 $ fmap (filter odd) (ship spentBot)
-      spentBot =
-        stepPainterBot (PainterBot blackShip North (sPos, sPos) startState)
+      spentBot   = stepPainterBot (PainterBot North (0, 0) startState M.empty)
       startState = I.emptyIntState { I.code = program, I.status = I.Running }
-      blackShip  = replicate maximumSideLength (replicate maximumSideLength 0)
-      sPos       = maximumSideLength `div` 2
